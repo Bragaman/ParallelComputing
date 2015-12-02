ab8f39f5bbe6be82b62ad3020421ce73e6f35b9f
Bragaman <cwbraga70@live.com>
2015-12-02 16:04:25 +0300

    add home task 1.2

--- hometask_1_2.c
+++ hometask_1_2.c
@@ -1,10 +1,21 @@
 //Написать на языке C программу, запрашивающую у пользователя три натуральных числа:
 //количество строк в матрице  A, количество столбцов в матрице A и количество столбцов в матрице B
 //и реализующее умножение матриц  A и B . Матрицы заполняются случайными целыми двухзначными числами.
+//Программа должна вычислять результирующую матрицу параллельно в нескольких процессах.
+//Количество процессов является параметром в программе и может быть легко изменено.
+//Матрицы должны хранится в разделяемой памяти.
 
-#include "stdio.h"
 #include <time.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_MODE (SHM_W | SHM_R |  IPC_CREAT)
 
 int* createRandomMatrix(int n, int m){
   int *matrix = (int *)malloc(n * m * sizeof(int));
@@ -41,9 +52,9 @@ int calcItem(int* matrixA, int* matrixB, int m, int n, int q, int x, int y){
   return tmp;
 }
 
-void calc(int* matrixA, int* matrixB, int* matrixC, int m, int n, int q){
-  int i=0;
-  for(i; i < m; i++){
+void calc(int* matrixA, int* matrixB, int* matrixC, int m, int n, int q, int processCount, int id){
+  int i=id;
+  for(i; i < m; i+=processCount){
     int j=0;
     for(j; j < q; j++){
       matrixC[i*q + j] = calcItem(matrixA, matrixB, m,n,q, j, i);
@@ -53,24 +64,55 @@ void calc(int* matrixA, int* matrixB, int* matrixC, int m, int n, int q){
 
 int main(int argc, char const *argv[]) {
 
-  int m=0 ,n=0 ,q=0 ; //first row, fisrt column, second column
+  int m=0 ,n=0 ,q=0, process=0; 
+  //first row, fisrt column, second column, count of process
   srand(time(NULL));
   printf("enter count of row first matrix: ");
-  scanf("%d",&m );
+  scanf("%d", &m);
   printf("enter count of column first matrix: ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   printf("enter count of column second matrix: ");
-  scanf("%d",&q );
+  scanf("%d", &q);
+  printf("enter count of process: ");
+  scanf("%d", &process);
 
   int* matrixA = createRandomMatrix(m, n);
   int* matrixB = createRandomMatrix(n, q);
-  int* matrixC = (int*)malloc(m*q*sizeof(int));
 
   printMatrix(matrixA, m, n);
   printMatrix(matrixB, n, q);
 
-  calc(matrixA, matrixB, matrixC, m, n, q);
+  
+  key_t key = 1995;
+  pid_t* pid = malloc(sizeof(pid_t) * process);
+  int shmid;
 
+  if ((shmid = shmget(key, m*q*sizeof(int), IPC_CREAT | 0666)) == -1){ 
+    printf("Unsuccessful memory get \n");
+    exit(-1);
+  }
+
+  int i;
+   for(i = 0; i < process; i++) {
+      pid[i] = fork();
+      if (pid[i] == 0)
+      {
+        int* ans = (int *)shmat(shmid, NULL, 0);
+        calc( matrixA, matrixB, ans, m,n,q, process, i);
+        return 0;
+      }
+   }
+  for(i = 0; i < process; i++) {
+      waitpid (pid[i], NULL, 0);
+  }
+
+  int* matrixC = (int*)shmat(shmid, NULL, 0);
   printMatrix(matrixC, m, q);
+  if (pid !=0)
+     if (shmctl(shmid,IPC_RMID,0) < 0) 
+      {  /* This destroys the memory queue */
+        printf("Problems destroying shared memory ID \n");
+        exit(-1);
+      }
   return 0;
 }
